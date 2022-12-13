89b5a22f2d9ef8774546c8e40c67338fd833e5b2
CryptoSquad <demasmamas@gmail.com>
2022-12-14 02:19:11 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [answer, setAnswer] = useState('')
+  const [request, setRequest] = useState(false)
+
+  const  paste = async () => {
+  try {
+    const clipboardItems = await navigator.clipboard.read();
+    for (const clipboardItem of clipboardItems) {
+      for (const type of clipboardItem.types) {
+        if(type === 'image/png') {
+          const blob = await clipboardItem.getType(type);
+          const file = new File([blob], 'screenshot');
+          sendScreenshot(file)
+        } 
+        else {
+          alert('Нет изображения в буфере обмена')
+        }
+        
+        }
+    }
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
+  }
+
+  const sendScreenshot = async (image) => {
+    const data = new FormData()
+    data.append('screenshot', image)
+    setRequest(true)
+    await fetch('http://158.160.45.164:3000/screenshot/add', {
+      method: 'POST',
+      body: data
+    }).then((res) => res.json()).then((json) => setAnswer(json.recognizedText));
+    setRequest(false)
+  }
+
+  // не сработало из-за безопасности браузера
+
+  // useEffect(() => {
+  //   window.addEventListener('keyup', (event) => {
+  //     if(event.keyCode === 44){
+  //       paste()
+  //     }
+  //   }
+  //   )
+  // },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Нажмите Print Screen</p>
+      <p>Нажмите отправить </p>
+      <p>(Да, это не одна кнопка 😢)</p>
+      <button type='button' onClick={paste} disabled={request}>Отправить</button>
+      {request && <p>Загрузка...</p>}
+      {answer && 
+        <div>
+          <p>Результат</p>
+          <p>{answer}</p>
+        </div>
+      }
     </div>
   );
 }
